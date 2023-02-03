7a3c109e6c11d2b7624895496522f582e0f70b12
nmpalaci <nickmpalacio@gmail.com>
2023-02-02 22:29:41 -0500

    working with user inputs and appending to url for api search

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 
-let boroughName
-let streetName
+
 
 
 function getData(){
 
+    let boroughName = document.getElementById('boroughInput').value
+    let reformattedBoroughName = capitalize(boroughName)
+    let streetName = document.getElementById('streetInput').value.toUpperCase()
     let resturantName = document.getElementById('restaurantInput').value.toUpperCase()
-    const url = 'https://data.cityofnewyork.us/resource/43nn-pn8j.json?' + 'dba=' + resturantName
+
+    //need to add in if statement to use different urls when user inputs all search terms, 2, or just 1
+    const url = 'https://data.cityofnewyork.us/resource/43nn-pn8j.json?' + 'dba=' + resturantName + '&boro=' + reformattedBoroughName
 
     fetch (url)
     .then((res) => res.json())
@@ -14,8 +18,16 @@ function getData(){
         console.log(data);
         console.log(resturantName)
         console.log(url)
+        
     })
+    .then(() => document.getElementById('restaurantInput').value = '')    
     .catch((err) => console.log('something went wrong', err))
 }
 
 document.getElementById("submit").addEventListener('click', getData)
+
+
+function capitalize(string){
+
+    return string.charAt(0).toUpperCase() + string.slice(1)
+}
