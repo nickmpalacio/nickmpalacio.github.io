31296df994f6ab6130c6707d6ebd64a008fbaeb8
nmpalaci <nickmpalacio@gmail.com>
2023-02-03 22:15:50 -0500

    added in support to pass name & borough to api and retrieve results

--- app.js
+++ app.js
@@ -4,20 +4,20 @@
 
 function getData(){
 
-    let boroughName = document.getElementById('boroughInput').value
+    let boroughName = document.getElementById('boroughInput').value.toString()
     let reformattedBoroughName = capitalize(boroughName)
     let streetName = document.getElementById('streetInput').value.toUpperCase()
     let resturantName = document.getElementById('restaurantInput').value.toUpperCase()
+    console.log(boroughName)
 
     //need to add in if statement to use different urls when user inputs all search terms, 2, or just 1
 
-    let url = 'https://data.cityofnewyork.us/resource/43nn-pn8j.json?' + 'dba=' + resturantName
+    let url
 
-
-    if (reformattedBoroughName =! null){
-       
+    if (reformattedBoroughName.value =! ''){
+         url = 'https://data.cityofnewyork.us/resource/43nn-pn8j.json?' + 'dba=' + resturantName + '&boro=' + reformattedBoroughName
     } else {
-      
+         url = 'https://data.cityofnewyork.us/resource/43nn-pn8j.json?' + 'dba=' + resturantName
     }
 
     fetch (url)
